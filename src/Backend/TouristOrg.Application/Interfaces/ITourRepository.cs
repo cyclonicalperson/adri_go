9cbd94d19870726de2e51cdd93d68956ee0579aa
Mladen Brankovic <mladenbrankovic1903@gmail.com>
2026-03-25 14:35:10 +0100

    odradjen backend za kreiranje tura

---
+++ src/Backend/TouristOrg.Application/Interfaces/ITourRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TouristOrg.Domain.Entities;
+
+namespace TouristOrg.Application.Interfaces;
+public interface ITourRepository
+{
+    Task<Tour?> GetByIdAsync(Guid id, CancellationToken ct = default);
+    Task<IEnumerable<Tour>> GetAllAsync(CancellationToken ct = default);
+    Task AddAsync(Tour tour, CancellationToken ct = default);
+    Task UpdateAsync(Tour tour, CancellationToken ct = default);
+    Task DeleteAsync(Guid id, CancellationToken ct = default);
+}
---
+++ src/Backend/TouristOrg.Domain/Enums/BookingStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouristOrg.Domain.Enums;
+public enum BookingStatus
+{
+    Pending = 0,
+    Confirmed = 1,
+    Cancelled = 2
+}
