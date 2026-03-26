cc1175c03b85049f3b0b9397ef7149754d629c04
Pavle <cyclonicalperson@gmail.com>
2026-03-26 13:27:56 +0100

    Dodati core modeli

---
+++ src/Frontend/src/app/core/models/activity.model.ts
@@ -0,0 +1,25 @@
+export type ActivityCategory =
+  | 'SPORT'
+  | 'SHOPPING'
+  | 'NIGHTLIFE'
+  | 'WELLNESS'
+  | 'DINING'
+  | 'SIGHTSEEING'
+  | 'ADVENTURE'
+  | 'CULTURE'
+  | 'OTHER';
+
+export interface Activity {
+  activityId: number;
+  name: string;
+  category: ActivityCategory;
+  description: string;
+}
+
+export interface CreateActivityRequest {
+  name: string;
+  category: ActivityCategory;
+  description: string;
+}
+
+export interface UpdateActivityRequest extends Partial<CreateActivityRequest> { }
