b28538457aaa0e05471df29d07636dd4007285ec
Pavle <cyclonicalperson@gmail.com>
2026-04-04 00:34:30 +0200

    Azurirane aktivnosti, njihove komponente, izgled i forma za dodavanje/izmenu

--- src/Frontend/Frontend_Admin/src/app/AdminAplikacija/aktivnosti/aktivnosti.routes.ts
+++ src/Frontend/Frontend_Admin/src/app/AdminAplikacija/aktivnosti/aktivnosti.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { AktivnostiListComponent } from './aktivnosti-list.component';
+import { AktivnostiListComponent } from './aktivnosti-list/aktivnosti-list.component';
+import { AktivnostFormComponent } from './aktivnosti-form/aktivnost-form.component';
 
 export const AKTIVNOSTI_ROUTES: Routes = [
-  {
-    path: '',
-    component: AktivnostiListComponent,
-  },
+  { path: '', component: AktivnostiListComponent },
+  { path: 'new', component: AktivnostFormComponent },
+  { path: ':id/edit', component: AktivnostFormComponent },
 ];
