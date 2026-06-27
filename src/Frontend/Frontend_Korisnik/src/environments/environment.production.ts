e19959a105bb48f0bdbc458c03dd9ca9cc38816b
Pavle <cyclonicalperson@gmail.com>
2026-06-27 02:08:18 +0200

    Zavrseno uklanjanje svih secreta i konfiguracija

--- src/Frontend/Frontend_Korisnik/src/environments/environment.production.ts
+++ src/Frontend/Frontend_Korisnik/src/environments/environment.production.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiUrl:       'https://softeng.pmf.kg.ac.rs:10185/api',
   touristAppUrl:'https://softeng.pmf.kg.ac.rs:10187',
   adminAppUrl:  'https://softeng.pmf.kg.ac.rs:10188',
-  googleClientId: '701119543171-s83jt4jk0lncvo69jtt6hh5jvp8k21ov.apps.googleusercontent.com',
+  googleClientId: '',
   mcpUrl:       'https://softeng.pmf.kg.ac.rs:10186/mcp',
   // Gemini chat ide isključivo kroz naš .NET backend — nikada direktno ka Gemini API-ju
   chatApiUrl:   'https://softeng.pmf.kg.ac.rs:10186/api/chat',
